ff6fe60024f168f3dc9979675c71920274cd8f5a
Kamila Raggi <kamilaraggi@hotmail.com>
2021-08-26 13:36:25 -0400

    script questions

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+//Kamila Raggi 04-Challenge Code Quiz
+
+var questions = [
+  {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+  },
+  {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+  },
+  {
+      title: "Arrays in Javascript can be used to store ____.",
+      choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+      answer: "all of the above"
+  },
+  {
+      title: "String values must be enclosed within ____ when being assigned to variables.",
+      choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+      answer: "quotes"
+  },
+  {
+      title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+      choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+      answer: "console log"
+  },
+
+];
+// Declared variables
+var score = 0;
+var questionIndex = 0;
+
